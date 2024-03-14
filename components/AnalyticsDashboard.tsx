7230f0e40ceb989c78e22f5fba33bb3e5be056d7
Moon-Knighte <githubanjan2255@gmail.com>
2024-03-14 12:10:52 +0545

    Rendering Avg. visitors/day on client side

--- components/AnalyticsDashboard.tsx
+++ components/AnalyticsDashboard.tsx
@@ -1,13 +1,23 @@
 "use client";
 import { Card } from "@tremor/react";
 
-const AnalyticsDashboard = () => {
+type AnalyticsDashboardProps = {
+  avgVisitorsPerDay: string;
+};
+
+const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
       <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
         <Card className="w-full mx-auto max-w-xs">
           <p className="text-tremor-default text-dark-tremor-content">
             Avg. visitors/day
+          </p>
+          <p
+            className="text-3xl text-tremor-background-emphasis
+          font-semibold"
+          >
+            {avgVisitorsPerDay}
           </p>
         </Card>
       </div>
